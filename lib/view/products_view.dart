a7e0c4a743f7050cf6fae460aa5ee6e3d689be3d
FranciscoVanegasG12 <91790409+FranciscoVanegasG12@users.noreply.github.com>
2021-11-25 20:16:11 -0500

    Add files via upload

---
+++ lib/view/products_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:tiendas_mobile/model/products.dart';
+import 'package:tiendas_mobile/persistence/product_dao.dart';
+
+class SecondRoute extends StatefulWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  _SecondRouteState createState() => _SecondRouteState();
+
+}
+class _SecondRouteState extends State<SecondRoute> {
+  //
+  final _products = ProductDAO.products;
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Productos"),
+      ),
+      body: _buildProductList(),
+    );
+  }
+  Widget _buildProductList() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: _products.length,
+        itemBuilder: (BuildContext _context, int i) {
+          return _buildRow(_products[i]);
+        }
+    );
+  }
+  Widget _buildRow(Product pr) {
+    return ListTile(
+      title: Text(pr.store, style: TextStyle(fontSize: 20, color: Colors.blue)),
+      subtitle:Text(pr.product, style: TextStyle(fontSize: 20, color: Colors.cyan)),
+    );
+  }
+}
---
+++ lib/view/stores_view.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:tiendas_mobile/model/store.dart';
+import 'package:tiendas_mobile/persistence/stores_dao.dart';
+import 'package:tiendas_mobile/view/products_view.dart';
+
+
+class StoresListView extends StatefulWidget {
+  const StoresListView({Key? key}) : super(key: key);
+
+  @override
+  _StoresListViewState createState() => _StoresListViewState();
+
+}
+
+class _StoresListViewState extends State<StoresListView> {
+//  final stDAO = StoreDAO();
+  final _stores = StoreDAO.stores;
+
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Listado de negocios en el barrio'),
+      ),
+      body: _buildStoresList(
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SecondRoute()),
+          );
+        },
+        label: const Text('Productos'),
+        icon: const Icon(Icons.add_shopping_cart_sharp),
+        backgroundColor: Colors.greenAccent,
+      ),
+    );
+
+  }
+
+
+  Widget _buildStoresList() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: _stores.length,
+        itemBuilder: (BuildContext _context, int i) {
+          return _buildRow(_stores[i]);
+        }
+    );
+  }
+
+  Widget _buildRow(Store st) {
+
+    return ListTile(
+      title: Text(st.name, style: TextStyle(fontSize: 20, color: Colors.blue)),
+      subtitle: Text(st.description,style: TextStyle(fontSize: 20, color: Colors.cyan)),
+      trailing: ElevatedButton(
+          onPressed: () {},
+          child: const Text('Visitar sitio del vendedor')
+      ),
+    );
+
+  }
+}
+
+
+
+
+
+
