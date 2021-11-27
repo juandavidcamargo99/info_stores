976c3924d8351bd47bbee5a34ec0f3b6c8c7bb83
Juan David Camargo Barajas <50251799+juandavidcamargo99@users.noreply.github.com>
2021-11-27 11:23:46 -0500

    Cambio de ruta de importaciones dao productos

--- lib/persistence/product_dao.dart
+++ lib/persistence/product_dao.dart
@@ -1,6 +1,6 @@
 import 'dart:convert' as JSON;
-import 'package:tiendas_mobile/model/products.dart';
-import 'package:tiendas_mobile/persistence/server_connection.dart';
+import 'package:info_stores/model/products.dart';
+import 'package:info_stores/persistence/server_connection.dart';
 
 class ProductDAO{
   static final List<Product> products = [];
