2053205574833b887b87981ca2cbc82dc9aa992b
FranciscoVanegasG12 <91790409+FranciscoVanegasG12@users.noreply.github.com>
2021-11-30 13:55:39 -0500

    Update stores_view.dart

--- lib/view/stores_view.dart
+++ lib/view/stores_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../model/store.dart';
 import '../persistence/store_dao.dart';
 import '../view/products_view.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 class StoresListView extends StatefulWidget {
@@ -58,7 +59,9 @@ class _StoresListViewState extends State<StoresListView> {
       title: Text(st.name, style: TextStyle(fontSize: 20, color: Colors.blue)),
       subtitle: Text(st.description,style: TextStyle(fontSize: 20, color: Colors.cyan)),
       trailing: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+          launch(st.url);
+          },
           child: const Text('Visitar sitio del vendedor')
       ),
     );
