c768ffe0f874d57b29225ef87f4f8d22800d82a3
FranciscoVanegasG12 <91790409+FranciscoVanegasG12@users.noreply.github.com>
2021-12-06 13:36:05 -0500

    Update products_view.dart

--- lib/view/products_view.dart
+++ lib/view/products_view.dart
@@ -3,39 +3,43 @@ import '../model/products.dart';
 import '../persistence/product_dao.dart';
 
 class SecondRoute extends StatefulWidget {
-  const SecondRoute({Key? key}) : super(key: key);
 
+  final List<Product> LstPr;
+
+  SecondRoute(this.LstPr);
   @override
-  _SecondRouteState createState() => _SecondRouteState();
 
+  _SecondRouteState createState() => _SecondRouteState();
 }
+
 class _SecondRouteState extends State<SecondRoute> {
   //
-  final _products = ProductDAO.products;
+  final _biggerFont = const TextStyle(fontSize: 18.0, color: Colors.blueAccent);
 
   @override
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Productos"),
+        title: const Text("Productos"),
       ),
       body: _buildProductList(),
     );
   }
+
   Widget _buildProductList() {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
-        itemCount: _products.length,
-        itemBuilder: (BuildContext _context, int i) {
-          return _buildRow(_products[i]);
-        }
+        itemCount: widget.LstPr.length,
+      itemBuilder: (context, i,) {return _buildRow(widget.LstPr[i]);}
     );
   }
+
   Widget _buildRow(Product pr) {
+
     return ListTile(
-      title: Text(pr.store, style: TextStyle(fontSize: 20, color: Colors.blue)),
-      subtitle:Text(pr.product, style: TextStyle(fontSize: 20, color: Colors.cyan)),
+      title: Text(pr.nombre, style: const TextStyle(fontSize: 20, color: Colors.blue)),
+      subtitle:Text(pr.unidad, style: const TextStyle(fontSize: 20, color: Colors.cyan)),
     );
   }
 }
