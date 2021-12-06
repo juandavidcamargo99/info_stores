a1e193afc916db058a82e6030dede2b279b75dc5
FranciscoVanegasG12 <91790409+FranciscoVanegasG12@users.noreply.github.com>
2021-12-06 13:40:36 -0500

    Update products.dart

--- lib/model/products.dart
+++ lib/model/products.dart
@@ -1,16 +1,17 @@
-class Product{
-  final String store;
-  final String product;
-  final String pdescription;
+class Product {
+  late int id;
+  late String nombre;
+  late String unidad;
+  late double precio;
 
-  Product(
-      this.store,
-      this.product,
-      this.pdescription,
-      );
-  Product.fromJson(Map<String, dynamic> json)
-  :
-        store = json['store'],
-        product = json['product'],
-        pdescription = json['pdescription'];
+  Product(this.id, this.nombre, this.unidad, this.precio);
+
+  //Texto = 100;Salchicon;GRAMO;1000
+  Product.fromString(String texto){
+    var list_pr = texto.split(";");
+    id = int.parse(list_pr[0]);
+    nombre = list_pr[1];
+    unidad = list_pr[2];
+    precio = double.parse(list_pr[3]);
+  }
 }
