d7266b7524b02f872d29cd1508ac6902fa05a011
FranciscoVanegasG12 <91790409+FranciscoVanegasG12@users.noreply.github.com>
2021-12-13 13:45:47 -0500

    Update home_view.dart

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'store.dart';
 import 'package:info_stores/view/products_view.dart';
 import 'stores_view.dart';
 import 'customer_form.dart';
@@ -11,6 +14,31 @@ class HomeView extends StatelessWidget {
     "images/buzon.png"
   ];
 
+  @override
+  void initState() {
+    FirebaseMessaging.instance.getInitialMessage();
+    FirebaseMessaging.onMessage.listen(
+        (message){
+          if(message.notification != null){
+            print(message.notification!.body);
+            print(message.notification!.title);
+          }
+          print(message);
+        },
+    );
+
+    FirebaseMessaging.onMessageOpenedApp.listen(
+
+          (messagge) {
+        final routeMessagge = messagge.data["route"];
+        print(routeMessagge);
+        Navigator.of(context).pushNamed(routeMessagge);
+      },
+
+    );
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
